d6ea5f6ff3b08a335a99801419180ebf1379c9f4
molecular <molecular@0x0000.de>
2017-01-20 20:14:57 +0100

    slowly...

--- soundcheck.js
+++ soundcheck.js
@@ -1,17 +1,19 @@
 #!/usr/bin/env node
 
-var program = require('commander');
+var cmd = require('commander');
+var config = require('./config');
 
-program
+cmd
   .version('0.1.0')
-  .option('-p, --peppers', 'Add peppers')
+  .option('-t, --torrent <url>', 'Download Torrent')
   .option('-P, --pineapple', 'Add pineapple')
   .option('-b, --bbq-sauce', 'Add bbq sauce')
   .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
   .parse(process.argv);
 
-console.log('you ordered a pizza with:');
-if (program.peppers) console.log('  - peppers');
-if (program.pineapple) console.log('  - pineapple');
-if (program.bbqSauce) console.log('  - bbq');
-console.log('  - %s cheese', program.cheese);
+if (cmd.torrent) {
+	console.log( 'torrents:', cmd.torrent );
+}
+
+console.log( "config: ", config );
+
